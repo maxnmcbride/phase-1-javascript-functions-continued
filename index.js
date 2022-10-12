3d6a9b47c1dfb79c449c15d64ea30353d71faf78
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-10-11 21:43:57 -0400

    commit tree 7f42f32e2ba02e78c7b54625f3d6a3939b3914e8

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun('read a book');
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork('drink water');
